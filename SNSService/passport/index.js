020f4fa51380702c6a627779f72740a8eb977173
seongsik <pss9205@gmail.com>
2021-05-25 23:13:10 +0900

    assignment#3 caching userinfo

--- SNSService/passport/index.js
+++ SNSService/passport/index.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const local = require("./localStrategy");
 const kakao = require("./kakaoStrategy");
 const User = require("../models/user");
-
+const UserDB = {};
 module.exports = () => {
   passport.serializeUser((user, done) => {
     console.log(user);
@@ -10,23 +10,30 @@ module.exports = () => {
   });
 
   passport.deserializeUser((id, done) => {
-    User.findOne({
-      where: { id },
-      include: [
-        {
-          model: User,
-          attributes: ["id", "nick"],
-          as: "Followers",
-        },
-        {
-          model: User,
-          attributes: ["id", "nick"],
-          as: "Followings",
-        },
-      ],
-    })
-      .then((user) => done(null, user))
-      .catch((err) => done(err));
+    if (id in UserDB) {
+      done(null, UserDB[id]);
+    } else {
+      User.findOne({
+        where: { id },
+        include: [
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followers",
+          },
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followings",
+          },
+        ],
+      })
+        .then((user) => {
+          UserDB[id] = user;
+          done(null, user);
+        })
+        .catch((err) => done(err));
+    }
   });
 
   local();
