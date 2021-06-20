8761db4307d21fad137d3effe6189d92f1b75414
seongsik <pss9205@gmail.com>
2021-06-21 00:13:25 +0900

    [UnitTest] add testing code auth.js

---
+++ SNSService/controllers/auth.js
@@ -0,0 +1,54 @@
+const User = require("../models/user");
+const cache = require("../passport/cache");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+exports.join = async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect("/join?error=exist");
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+exports.login = (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+};
+
+exports.logout = (req, res) => {
+  cache.remove(req.user.id);
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
+};
+
+exports.kakaocallback = (req, res) => {
+  res.redirect("/");
+};
