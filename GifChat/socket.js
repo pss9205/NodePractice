534d6862b3b2a17a3d60327a7fd438191d0649c9
seongsik <pss9205@gmail.com>
2021-07-18 23:21:14 +0900

    [gifchat][assignment2] add session to axios request

--- GifChat/socket.js
+++ GifChat/socket.js
@@ -1,11 +1,28 @@
 const SocketIO = require("socket.io");
 const axios = require("axios");
 const sharedsession = require("express-socket.io-session");
+const cookie = require("cookie-signature");
+const cookieParser = require("cookie-parser");
 module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: "/socket.io" });
   app.set("io", io);
   const room = io.of("/room");
   const chat = io.of("/chat");
+
+  room.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(
+      socket.request,
+      socket.request.res,
+      next
+    );
+  });
+  chat.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(
+      socket.request,
+      socket.request.res,
+      next
+    );
+  });
   //socket.io 버전 변경으로 인해 안되는 부분
   //express-socket.io-session 패키지로 session 사용 가능
   //io.use가 아닌 각 네임스페이스의 use로 해야함
@@ -27,8 +44,20 @@ module.exports = (server, app, sessionMiddleware) => {
     referes = referer.split("/");
     const roomId = referes[referes.length - 1].replace(/\?.+/, "");
     socket.join(roomId);
+    const signedCookie = req.signedCookies["connect.sid"];
+    const connectSID = encodeURIComponent(
+      "s:" + cookie.sign(signedCookie, process.env.COOKIE_SECRET)
+    );
     axios
-      .post(`http://localhost:8005/room/connect/${roomId}`)
+      .post(
+        `http://localhost:8005/room/connect/${roomId}`,
+        {},
+        {
+          headers: {
+            Cookie: `connect.sid=${connectSID}`,
+          },
+        }
+      )
       .then(() => {
         console.log(`user entered to {roomId}`);
       })
@@ -39,7 +68,15 @@ module.exports = (server, app, sessionMiddleware) => {
       console.log("disconnection namespace:chat");
       socket.leave(roomId);
       axios
-        .post(`http://localhost:8005/room/disconnect/${roomId}`)
+        .post(
+          `http://localhost:8005/room/disconnect/${roomId}`,
+          {},
+          {
+            headers: {
+              Cookie: `connect.sid=${connectSID}`,
+            },
+          }
+        )
         .then(() => {
           console.log(`user left {roomId}`);
         })
